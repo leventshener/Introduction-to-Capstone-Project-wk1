43a5a5c6eb6ca0de6890ec9b8af2f640c9b7b188
leventshener <83645430+leventshener@users.noreply.github.com>
2021-05-04 16:00:21 +0200

    Add the assignment
    add the assignment of applied data science capstone

---
+++ Assignment of Introduction to Capstone Project wk1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#this notebook will be mainly used for the capstone project.
+import pandas as pd
+import numpy as nm
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
